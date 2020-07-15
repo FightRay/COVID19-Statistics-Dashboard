aba7d2d4e01955539858ed5ab79428e884d3197f
Ray <fightxray@gmail.com>
2020-07-15 03:21:25 +0300

    Build folder and updated README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # COVID19-STATUS-DASHBOARD
 An interactive Dashboard for viewing COVID19 statistics. Made for the 'Workiz' exam.
+
+Production version available in this temporary link:
+https://test.rey.network/
---
+++ build/precache-manifest.2175d3a1e6baee2479597e98f5880c65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2460e39aa3b1e5f50481e0d62241973f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80b5aa2dee4c7fe18496",
+    "url": "/static/css/main.0d645721.chunk.css"
+  },
+  {
+    "revision": "6799746d79d767fc0f20",
+    "url": "/static/js/2.ea4ce260.chunk.js"
+  },
+  {
+    "revision": "2e4f31804d72137a9d9b13db1a0e4557",
+    "url": "/static/js/2.ea4ce260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80b5aa2dee4c7fe18496",
+    "url": "/static/js/main.4ec5424e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "f93143ece2d1024fdb586ea1ba28d755",
+    "url": "/static/media/logo.f93143ec.svg"
+  }
+]);
